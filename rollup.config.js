32c22c7532c73496c26a330412cf481a401678f9
Ryan Pitts <ryanpitts@protonmail.com>
2020-01-05 01:36:55 -0400

    tailwind css

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -22,7 +23,8 @@ export default {
 			// a separate file — better for performance
 			css: css => {
 				css.write('public/build/bundle.css');
-			}
+      },
+      preprocess: sveltePreprocess({ postcss: true }), 
 		}),
 
 		// If you have external dependencies installed from
